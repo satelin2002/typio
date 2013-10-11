27fd08f0695a216c2231ce57eb45282ada2c62d5
Sarvesh Telang <satelin2002@gmail.com>
2013-10-11 09:42:51 -0700

    fonts precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Typio::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
+  #config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
