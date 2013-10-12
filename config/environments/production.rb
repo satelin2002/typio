6091cd91c100ee6603c6681deef7e0d66e997c72
Sarvesh Telang <satelin2002@gmail.com>
2013-10-11 22:45:55 -0700

    Config assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Typio::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  config.font_assets.origin = "http://mysterious-ocean-8923.herokuapp.com"
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
