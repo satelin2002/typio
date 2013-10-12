dd03fe0f2fcf58da7dba88925c789858e51da5f4
Sarvesh Telang <satelin2002@gmail.com>
2013-10-12 00:07:54 -0700

    Added s3 font changs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,5 +29,5 @@ Typio::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
-  config.font_assets.origin = "http://localhost:3000"
+  config.font_assets.origin = "http://s3.amazonaws.com"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Typio::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   
-  config.font_assets.origin = "http://mysterious-ocean-8923.herokuapp.com"
-
+  config.font_assets.origin = "http://s3.amazonaws.com"
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
